3826bc3f8c626dab4068fabde24c5842f15ee9c2
Ashish Bansod <86002886+bansodji@users.noreply.github.com>
2022-09-14 11:42:44 +0530

    Routes edited for github hosting

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const App = () => {
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/Hanzo-Hasashi/" element={<Home />} />
       </Routes>
       <footer className=' bg-3 pt-5 pb-4 fff overflow-hidden'>
         <Footer />
